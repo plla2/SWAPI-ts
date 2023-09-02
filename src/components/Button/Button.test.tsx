db59c547004e462ca0558c87a5c0aef3ae601f01
plla2 <jhbljs92@naver.com>
2023-09-03 02:38:03 +0900

    ✅ 버튼, 헤더 테스트 코드

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -1,4 +1,19 @@
-test("Example test", () => {
-  // 테스트 코드 작성
-  expect(2 + 2).toBe(4);
+import { fireEvent, render, screen } from "@testing-library/react";
+import Button from "./Button";
+
+describe("모달 버튼 테스트", () => {
+  test("로그인 모달", () => {
+    const handleOpenLogin = jest.fn();
+    render(<Button open={handleOpenLogin} text="log in" />);
+    const loginButton = screen.getByText(/log in/i);
+    fireEvent.click(loginButton);
+    expect(handleOpenLogin).toHaveBeenCalledTimes(1);
+  });
+  test("회원가입 모달", () => {
+    const handleOpenSignup = jest.fn();
+    render(<Button open={handleOpenSignup} text="sign up" />);
+    const signupButton = screen.getByText(/sign up/i);
+    fireEvent.click(signupButton);
+    expect(handleOpenSignup).toHaveBeenCalledTimes(1);
+  });
 });
