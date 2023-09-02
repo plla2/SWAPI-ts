6637ee309013f9e4a1022e2c522b0cf7d455021f
plla2 <jhbljs92@naver.com>
2023-09-02 22:19:55 +0900

    ♻️ 테스트 코드 중복 리팩토링

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,51 +4,40 @@ import userEvent from "@testing-library/user-event";
 import StarShip from "./pages/StarShip";
 import Home from "./pages/Home";
 import Actors from "./pages/Actors";
+import React from "react";
 
-test("첫화면 렌더링", () => {
+const renderWithRouter = (initialPath: string, component: React.ReactNode) => {
   render(
-    <MemoryRouter initialEntries={["/"]}>
+    <MemoryRouter initialEntries={[initialPath]}>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path={initialPath} element={component} />
       </Routes>
     </MemoryRouter>
   );
-  const homeView = screen.getByText(/안녕 나는 홈페이지야/i);
-  expect(homeView).toBeInTheDocument();
-});
+};
 
-test("starships 경로 설정", () => {
-  // 초기 경로를 "/starships"로 설정하여 StarShip 컴포넌트가 렌더링되도록 합니다.
-  render(
-    <MemoryRouter initialEntries={["/starships"]}>
-      <Routes>
-        <Route path="/starships" element={<StarShip />} />
-      </Routes>
-    </MemoryRouter>
+const navigateToPathAndCheckLabel = (path: string, label: string) => {
+  renderWithRouter(
+    path,
+    path === "/" ? <Home /> : path === "/starships" ? <StarShip /> : <Actors />
   );
-  // "starships" 텍스트를 가진 엘리먼트를 클릭하여 "/starships"로 이동합니다.
-  userEvent.click(screen.getByText(/starships/i));
-
-  // "/starships" 경로로 이동한 후 "starships list" label을 검증합니다.
+  userEvent.click(screen.getByText(label, { exact: false }));
   expect(
-    screen.getByRole("list", { name: /starships list/i })
+    screen.getByRole("list", { name: `${label} list` })
   ).toBeInTheDocument();
-});
+};
 
-test("actors 경로 설정", () => {
-  // 초기 경로를 "/actors"로 설정하여 Actors 컴포넌트가 렌더링되도록 합니다.
-  render(
-    <MemoryRouter initialEntries={["/actors"]}>
-      <Routes>
-        <Route path="/actors" element={<Actors />} />
-      </Routes>
-    </MemoryRouter>
-  );
-  // "actors" 텍스트를 가진 엘리먼트를 클릭하여 "/actors"로 이동합니다.
-  userEvent.click(screen.getByText(/actors/i));
+describe("페이지 렌더링 테스트", () => {
+  test("첫화면 렌더링", () => {
+    renderWithRouter("/", <Home />);
+    expect(screen.getByText(/안녕 나는 홈페이지야/i)).toBeInTheDocument();
+  });
 
-  // "/actors" 경로로 이동한 후 "actors list" label을 검증합니다.
-  expect(
-    screen.getByRole("list", { name: /actors list/i })
-  ).toBeInTheDocument();
+  test("starships 경로 설정", () => {
+    navigateToPathAndCheckLabel("/starships", "starships");
+  });
+
+  test("actors 경로 설정", () => {
+    navigateToPathAndCheckLabel("/actors", "actors");
+  });
 });
