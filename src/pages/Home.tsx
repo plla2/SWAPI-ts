02d95fbd679c31f5916b1076e5c03aec215eb398
plla2 <jhbljs92@naver.com>
2023-09-04 00:33:28 +0900

    📝 Home tsx UI작성

--- src/pages/Home.tsx
+++
@@ -1,5 +0,0 @@
-const Home = () => {
-  return <div>안녕 나는 홈페이지야</div>;
-};
-
-export default Home;
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,34 @@
+import mainImg from "../../assets/mainImg.webp";
+import { Link } from "react-router-dom";
+import { cardsData } from "../../constant/Const";
+import Card from "../../components/Card/Card";
+import "./Home.scss";
+import ImageSlider from "../../components/ImageSlider/ImageSlider";
+
+const Home = () => {
+  return (
+    <>
+      <ImageSlider />
+      <section className="cards">
+        <div className="cards__header">
+          <img src={mainImg} alt="메인이미지" className="cards__image" />
+          <div className="cards__header-info">
+            <h2>A new era of Star Wars.</h2>
+            <Link to="#!">START</Link>
+          </div>
+        </div>
+        <div className="cards__body">
+          {cardsData.map((card, index) => {
+            return (
+              <Link to={card.link} key={index}>
+                <Card text={card.text} image={card.image} />
+              </Link>
+            );
+          })}
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Home;
