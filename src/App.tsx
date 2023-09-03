5191740cdfa107918da5e1b76cfce72e4773d431
plla2 <jhbljs92@naver.com>
2023-09-04 00:28:06 +0900

    📝 sessionStorage, Detail 컴포넌트 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Nav from "./components/Nav/Nav";
-import Home from "./pages/Home";
+import Home from "./pages/Home/Home";
 import StarShip from "./pages/StarShip";
 import Actors from "./pages/Actors";
+import { useEffect, useState } from "react";
+import Details from "./components/Details/Details";
 
 function App() {
+  const [starshipId, setStarShipId] = useState();
+  const [selectedStarship, setSelectedStarship] = useState();
+
+  useEffect(() => {
+    sessionStorage.setItem("starship", JSON.stringify(selectedStarship));
+    sessionStorage.setItem("id", JSON.stringify(starshipId));
+  }, [selectedStarship, starshipId]);
+  console.log(starshipId);
+
   return (
     <BrowserRouter>
       <>
@@ -13,7 +24,19 @@ function App() {
         <Nav />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/starships" element={<StarShip />} />
+          <Route
+            path="/starships"
+            element={
+              <StarShip
+                changeStarship={setSelectedStarship}
+                changeId={setStarShipId}
+              />
+            }
+          />
+          <Route
+            path="/starships/:id"
+            element={<Details info={selectedStarship} id={starshipId} />}
+          />
           <Route path="/actors" element={<Actors />} />
         </Routes>
       </>
