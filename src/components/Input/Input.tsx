0fb868f3ea94574ec718060a4f5363e31bb62841
plla2 <jhbljs92@naver.com>
2023-09-16 21:34:00 +0900

    ✨ Input 컴포넌트 생성

---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Input = ({ type, id, text, validation, error }) => {
+  return (
+    <>
+      <input
+        type={type}
+        name={id}
+        id={id}
+        aria-required="true"
+        maxLength={60}
+        className={error ? `input input-error` : "input"}
+        placeholder={text}
+        onBlur={validation}
+      />
+      <p className="input__errors">{error}</p>
+    </>
+  );
+};
+
+export default Input;
